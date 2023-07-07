96dfe3de0a160e144d5b7d55ac2261267416cb4c
Rei-Suzuki1729 <eva2021end@gmail.com>
2023-07-07 11:02:43 +0900

    station6&7

--- model/todo.go
+++ model/todo.go
@@ -1,13 +1,28 @@
 package model
 
+import 	(
+	"time"
+)
+
 type (
 	// A TODO expresses ...
-	TODO struct{}
+	TODO struct{
+		ID          int       `json:"id" db:"id"`
+		Subject     string    `json:"subject" db:"subject"`
+		Description string    `json:"description" db:"description"`
+		CreatedAt   time.Time `json:"created_at" db:"created_at"`
+		UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+	}
 
 	// A CreateTODORequest expresses ...
-	CreateTODORequest struct{}
+	CreateTODORequest struct{
+		Subject     string `json:"subject"`
+		Description string `json:"description"`
+	}
 	// A CreateTODOResponse expresses ...
-	CreateTODOResponse struct{}
+	CreateTODOResponse struct{
+		TODO TODO `json:"todo"`
+	}
 
 	// A ReadTODORequest expresses ...
 	ReadTODORequest struct{}
